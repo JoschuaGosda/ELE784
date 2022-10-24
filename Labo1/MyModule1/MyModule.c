4e82692a3dc50934057e470b11781385d3d27275
Joschua Gosda <joschuagosda@Joschuas-MacBook-Air.local>
2022-10-23 20:49:22 -0400

    implemented open & release functions without having it tested yet

--- Labo1/MyModule1/MyModule.c
+++ Labo1/MyModule1/MyModule.c
@@ -1,4 +1,5 @@
 #include "MyModule.h"
+#include <stdbool.h>
 
 MODULE_AUTHOR("Bruno");
 MODULE_LICENSE("Dual BSD/GPL");
@@ -20,9 +21,47 @@ struct file_operations MyModule_fops = {
 	.release	=	MyModule_release
 };
 
+uint16_t owner; // takes the owners id
+bool READ, WRITE;
+
+static spinlock_t lock_access = SPIN_LOCK_UNLOCKED;
+
+
+
 struct cdev My_cdev;
 
 static ssize_t MyModule_read(struct file *file, char *buff , size_t len, loff_t *off) {
+
+  // lock to protect critical code section
+  spin_lock(&lock_access);
+  // get the owner if nothing is accessed yet
+  if (!(WRITE && READ)){
+    owner = current->uid;
+  }
+
+  // check if the user that tries to open is the owner
+  if (current->uid == owner){
+    // check the access mode, update READ, WRITE flags or return error if applicable
+    if ((filp->f_flags & O_ACCMODE) == O_WRONLY && !WRITE) {
+        WRITE = TRUE;
+        spin_unlock(&lock_access);
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && !READ) {
+        READ = TRUE;
+        spin_unlock(&lock_access);
+    } else if ((filp->f_flags & O_ACCMODE) == RDWR && (!WRITE && !READ)) {
+        WRITE = TRUE;
+        READ = TRUE;
+        spin_unlock(&lock_access);
+    } else {
+      spin_unlock(&lock_access);
+      return -ENOOTY;
+    }
+  } else { // current user it not the owner
+    spin_unlock(&lock_access);
+    return -ENOOTY;
+  }
+
+  // TODO: Port Série doit être placé en mode Réception
 
   printk(KERN_WARNING"Bonjour les amis, READ\n");
 
@@ -48,6 +87,24 @@ static int MyModule_open(struct inode *inode, struct file *filp) {
 
 static int MyModule_release(struct inode *inode, struct file *filp) {
 
+  spin_lock(&lock_access);
+  if ((filp->f_flags & O_ACCMODE) == O_WRONLY && WRITE) {
+        WRITE = FALSE;
+        spin_unlock(&lock_access);
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && READ) {
+        READ = FALSE;
+        spin_unlock(&lock_access);
+    } else if ((filp->f_flags & O_ACCMODE) == RDWR && (WRITE && READ)) {
+        WRITE = FALSE;
+        READ = FALSE;
+        spin_unlock(&lock_access);
+    } else {
+      spin_unlock(&lock_access);
+      return -ENOOTY;
+    }
+
+  // TODO: Et si le mode d’ouverture était O_RDONL Y ou O_RDWR, la Réception du Port Série doit être interrompue afin d’arrêter de recevoir des données
+
   printk(KERN_WARNING"Bonjour les amis, RELEASE\n");
 
   return 0;
@@ -66,6 +123,10 @@ int n;
 
   cdev_init(&My_cdev, &MyModule_fops);
   cdev_add(&My_cdev, My_dev, 4); 
+
+  // set read and write flags to FALSE 
+  READ = FALSE;
+  WRITE = FALSE;
 
   printk(KERN_WARNING"MyMod : Hello World ! MyModule_X = %u\n", MyModule_X);
 
