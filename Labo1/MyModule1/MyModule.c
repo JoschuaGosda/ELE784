3a3b5e7c98e745510b6cdd2e9f98a572f34116f8
Leo Lemieux <leo.lemieux-girard.1@ens.etsmtl.ca>
2022-10-24 19:08:22 -0400

    port number is check user work. STILL PROBLEM warning msg are faulty

--- Labo1/MyModule1/MyModule.c
+++ Labo1/MyModule1/MyModule.c
@@ -23,8 +23,11 @@ struct file_operations MyModule_fops = {
 	.release	=	MyModule_release
 };
 
-uint16_t owner; // takes the owners id
-bool file_read, file_write;
+uint16_t owner[PORTNUMBER]; // takes the owners id
+bool file_read[PORTNUMBER], file_write[PORTNUMBER];
+//uint16_t owner;
+//uint8_t idMinor
+//bool file_read, file_write;
 
 struct cdev My_cdev;
 
@@ -47,27 +50,31 @@ static ssize_t MyModule_write(struct file *filp, const char *buff , size_t len, 
 
 static int MyModule_open(struct inode *inode, struct file *filp) {
 		//numPort = inode->i_rdev;
-
+    uint32_t numPort; 
   printk(KERN_WARNING"MyMod: OPEN\n");
   // lock to protect critical code section
   spin_lock(&lock_access);
+  // get the port the owner want to joint	
+  numPort = MINOR(inode->i_rdev);
+  
+printk(KERN_WARNING"MyMod open: ir_dev : %u\n",numPort);
   // get the owner if nothing is accessed yet
-  if (!file_write && !file_read){
-    owner = current_uid().val;
+  if (!file_write[numPort] && !file_read[numPort]){
+    owner[numPort] = current_uid().val;
   }
 
   // check if the user that tries to open is the owner
-  if (current_uid().val == owner){
+  if (current_uid().val == owner[numPort]){
     // check the access mode, update file_read, file_write flags or return error if applicable
-    if ((filp->f_flags & O_ACCMODE) == O_WRONLY && !file_write) {
-        file_write = true;
+    if ((filp->f_flags & O_ACCMODE) == O_WRONLY && !file_write[numPort]) {
+        file_write[numPort] = true;
         spin_unlock(&lock_access);
-    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && !file_read) {
-        file_read = true;
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && !file_read[numPort]) {
+        file_read[numPort] = true;
         spin_unlock(&lock_access);
-    } else if ((filp->f_flags & O_ACCMODE) == O_RDWR && (!file_write && !file_read)) {
-        file_write = true;
-        file_read = true;
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDWR && (!file_write[numPort] && !file_read[numPort])) {
+        file_write[numPort] = true;
+        file_read[numPort] = true;
         spin_unlock(&lock_access);
     } else {
       spin_unlock(&lock_access);
@@ -81,7 +88,7 @@ static int MyModule_open(struct inode *inode, struct file *filp) {
   // TODO: Port Série doit être placé en mode Réception
 
 
-  printk(KERN_WARNING"MyMod (end Open): file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
+  printk(KERN_WARNING"MyMod (end Open): file_read[%lu]: %d file_write: %d owner: %d \n",numPort, file_read[numPort], file_write[numPort], owner[numPort]);
 
   return 0;
 
@@ -89,16 +96,22 @@ static int MyModule_open(struct inode *inode, struct file *filp) {
 
 static int MyModule_release(struct inode *inode, struct file *filp) {
   printk(KERN_WARNING"MyMod: RELEASE\n");
+
+    uint32_t numPort; 
+
+  // get the port the owner want to joint	
+  numPort = MINOR(inode->i_rdev);
+
   spin_lock(&lock_access);
-  if ((filp->f_flags & O_ACCMODE) == O_WRONLY && file_write) {
-        file_write = false;
+  if ((filp->f_flags & O_ACCMODE) == O_WRONLY && file_write[numPort]) {
+        file_write[numPort] = false;
         spin_unlock(&lock_access);
-    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && file_read) {
-        file_read = false;
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDONLY && file_read[numPort]) {
+        file_read[numPort] = false;
         spin_unlock(&lock_access);
-    } else if ((filp->f_flags & O_ACCMODE) == O_RDWR && (file_write && file_read)) {
-        file_write = false;
-        file_read = false;
+    } else if ((filp->f_flags & O_ACCMODE) == O_RDWR && (file_write[numPort] && file_read[numPort])) {
+        file_write[numPort] = false;
+        file_read[numPort] = false;
         spin_unlock(&lock_access);
     } else {
       spin_unlock(&lock_access);
@@ -108,7 +121,7 @@ static int MyModule_release(struct inode *inode, struct file *filp) {
   // TODO: Et si le mode d’ouverture était O_RDONL Y ou O_RDWR, la Réception du Port Série doit être interrompue afin d’arrêter de recevoir des données
 
 
-  printk(KERN_WARNING"MyMod: (end Release) file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
+  printk(KERN_WARNING"MyMod: (end Release) file_read[%lu]: %d file_write: %d owner: %d \n",numPort, file_read, file_write, owner);
 
   return 0;
 
@@ -123,13 +136,23 @@ int n;
   for (n = 0; n < PORTNUMBER; n++) {
   	device_create(MyClass, NULL, (My_dev + n), NULL, "MyModuleNode%d", n);
   }
+  printk(KERN_WARNING"MyMod init : My_dev0 : %u , My_dev1 : %u ",My_dev,My_dev+1);
+  uint16_t a = MAJOR(My_dev);
+  uint16_t b = MINOR(My_dev);
+  uint16_t aa = MAJOR(My_dev+1);
+  uint16_t bb = MINOR(My_dev+1);
 
+  printk(KERN_WARNING"MyMod DEV0 major :%u, minor: %u DEV1 maj :%u minor:%u ", a,b,aa,bb);
   cdev_init(&My_cdev, &MyModule_fops);
   cdev_add(&My_cdev, My_dev, PORTNUMBER); 
 
   // set file_read and file_write flags to FALSE 
-  file_read = false;
-  file_write = false;
+  //todo loop with PORTNUMBER
+
+  file_read[0] = false;
+  file_write[0] = false;
+  file_read[1] = false;
+  file_write[1] = false;
 
   printk(KERN_WARNING"MyMod : Hello World ! MyModule_X = %u\n", MyModule_X);
 
