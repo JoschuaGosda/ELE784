3c72419ae66aa2c9306c23ea408c483bc7df52a7
Leo Lemieux <leo.lemieux-girard.1@ens.etsmtl.ca>
2022-10-24 18:06:55 -0400

    working check point, next is i_rdev functionality

--- Labo1/MyModule1/MyModule.c
+++ Labo1/MyModule1/MyModule.c
@@ -1,8 +1,8 @@
 #include "MyModule.h"
 
 DEFINE_SPINLOCK(lock_access);
-
-
+#define PORTNUMBER 2
+ 
 MODULE_AUTHOR("Bruno");
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -31,26 +31,28 @@ struct cdev My_cdev;
 static ssize_t MyModule_read(struct file *filp, char *buff , size_t len, loff_t *off) {
 
 
-  printk(KERN_WARNING"Bonjour les amis, READ\n");
+  printk(KERN_WARNING"MyMod: READ\n");
 
   return 0;
-
+ 
 }
 
 static ssize_t MyModule_write(struct file *filp, const char *buff , size_t len, loff_t *off) {
 
-  printk(KERN_WARNING"Bonjour les amis, WRITE\n");
+  printk(KERN_WARNING"MyMod: WRITE\n");
 
   return 0;
 
 }
 
 static int MyModule_open(struct inode *inode, struct file *filp) {
+		//numPort = inode->i_rdev;
 
+  printk(KERN_WARNING"MyMod: OPEN\n");
   // lock to protect critical code section
   spin_lock(&lock_access);
   // get the owner if nothing is accessed yet
-  if (!file_write && !file_read)){
+  if (!file_write && !file_read){
     owner = current_uid().val;
   }
 
@@ -78,15 +80,15 @@ static int MyModule_open(struct inode *inode, struct file *filp) {
 
   // TODO: Port Série doit être placé en mode Réception
 
-  printk(KERN_WARNING"Bonjour les amis, OPEN\n");
-  printk(KERN_WARNING"file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
+
+  printk(KERN_WARNING"MyMod (end Open): file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
 
   return 0;
 
 }
 
 static int MyModule_release(struct inode *inode, struct file *filp) {
-
+  printk(KERN_WARNING"MyMod: RELEASE\n");
   spin_lock(&lock_access);
   if ((filp->f_flags & O_ACCMODE) == O_WRONLY && file_write) {
         file_write = false;
@@ -105,8 +107,8 @@ static int MyModule_release(struct inode *inode, struct file *filp) {
 
   // TODO: Et si le mode d’ouverture était O_RDONL Y ou O_RDWR, la Réception du Port Série doit être interrompue afin d’arrêter de recevoir des données
 
-  printk(KERN_WARNING"Bonjour les amis, RELEASE\n");
-  printk(KERN_WARNING"file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
+
+  printk(KERN_WARNING"MyMod: (end Release) file_read: %d file_write: %d owner: %d \n", file_read, file_write, owner);
 
   return 0;
 
@@ -115,15 +117,15 @@ static int MyModule_release(struct inode *inode, struct file *filp) {
 static int __init mod_init (void) {
 int n;
 
-  alloc_chrdev_region (&My_dev,  0,  4, "MonPremierPilote");
+  alloc_chrdev_region (&My_dev,  0,  PORTNUMBER, "MonPremierPilote");
 
   MyClass = class_create(THIS_MODULE, "MyModule");
-  for (n = 0; n < 4; n++) {
+  for (n = 0; n < PORTNUMBER; n++) {
   	device_create(MyClass, NULL, (My_dev + n), NULL, "MyModuleNode%d", n);
   }
 
   cdev_init(&My_cdev, &MyModule_fops);
-  cdev_add(&My_cdev, My_dev, 4); 
+  cdev_add(&My_cdev, My_dev, PORTNUMBER); 
 
   // set file_read and file_write flags to FALSE 
   file_read = false;
@@ -139,12 +141,12 @@ int n;
 
   cdev_del(&My_cdev);
 
-  for (n = 0; n < 4; n++) {
+  for (n = 0; n < PORTNUMBER; n++) {
   	device_destroy(MyClass, (My_dev + n));
   }
   class_destroy(MyClass);
 
-  unregister_chrdev_region(My_dev, 4);
+  unregister_chrdev_region(My_dev, PORTNUMBER);
 
   printk(KERN_WARNING"MyMod : Goodbye cruel World !\n");
 
@@ -153,11 +155,11 @@ int n;
 module_init(mod_init);
 module_exit(mod_exit);
 
-// make -C /usr/src/linux-headers-4.15.18 M=`pwd` modules
+// make -C /usr/src/linux-source-4.15.18 M=`pwd` modules
 // sudo insmod ./MyModule.ko
 // sudo rmmod MyModule
 // lsmod | grep MyModule
-
+///var/log/ dmesg | grep MyMod
 
 
 
--- Labo1/Test/testopen1.c
+++ Labo1/Test/testopen1.c
@@ -8,14 +8,14 @@
 #define DEFAULT_SERIALPORT	"/dev/SerialDev_Node0"
 
 int main() {
-	   int 	fd;
+	   int 	fd,fd1;
 	   char tp = 0;
 	   int 	ret;
 
 	   
 		  
 	   
-	fd = open("/dev/MyModuleNode0", O_RDWR);
+	fd = open("/dev/MyModuleNode0", O_RDONLY);
 	   if (fd < 0) {
 		   printf("Erreur d'ouverture = %d\n", fd);
 		   return -1;
@@ -24,6 +24,41 @@ int main() {
 
 	   close(fd);
 	   printf("\n");
+
+
+	fd = open("/dev/MyModuleNode0", O_WRONLY);
+	   if (fd < 0) {
+		   printf("Erreur d'ouverture = %d\n", fd);
+		   return -1;
+	   }
+
+
+	   close(fd);
+	   printf("\n");
+
+
+	fd = open("/dev/MyModuleNode0", O_RDWR);
+	   if (fd < 0) {
+		   printf("Erreur d'ouverture = %d\n", fd);
+		   return -1;
+	   }
+
+
+
+
+
+	fd1 = open("/dev/MyModuleNode1", O_RDWR);
+	   if (fd1 < 0) {
+		   printf("Erreur d'ouverture1 = %d\n", fd1);
+		   return -1;
+	   }
+
+
+	   close(fd);
+	   printf("\n");
+	   close(fd1);
+	   printf("\n");
+
 
 	   return EXIT_SUCCESS;
 }
