9c6add25fe722171030cd01822ef5788bdafaf4b
Joschua Gosda <joschuagosda@Joschuas-MacBook-Air.local>
2022-10-27 18:47:28 -0400

    filp working, next: access protection using filp flags

--- Labo1/MyModule1/MyModule.c
+++ Labo1/MyModule1/MyModule.c
@@ -2,7 +2,8 @@
 
 DEFINE_SPINLOCK(lock_access);
 #define PORTNUMBER 2
-#define BUFFERCAPACITY 128
+#define BUFFER_CAPACITY 128
+#define BUFFER_ELEMENTSIZE 8
 
 
 MODULE_AUTHOR("Bruno");
@@ -19,9 +20,9 @@ struct class *MyClass;
 
 struct file_operations MyModule_fops = {
 	.owner		=	THIS_MODULE,
-	.read		=	MyModule_read,
+	.read		  =	MyModule_read,
 	.write		=	MyModule_write,
-	.open		=	MyModule_open,
+	.open		  =	MyModule_open,
 	.release	=	MyModule_release
 };
 
@@ -37,7 +38,8 @@ struct cdev My_cdev;
 
 
 static int __init mod_init (void) {
-int n, i;
+
+  int n;
 
   alloc_chrdev_region (&My_dev,  0,  PORTNUMBER, "MonPremierPilote");
 
@@ -46,10 +48,12 @@ int n, i;
   	device_create(MyClass, NULL, (My_dev + n), NULL, "MyModuleNode%d", n);
   }
 
-
   cdev_init(&My_cdev, &MyModule_fops);
   cdev_add(&My_cdev, My_dev, PORTNUMBER); 
 
+  for (n = 0; n < PORTNUMBER; n++){
+    cb_init(&pdata[n].buf_rdwr, BUFFER_CAPACITY, BUFFER_ELEMENTSIZE);
+  }
 
   printk(KERN_WARNING"MyMod : Hello World ! MyModule_X = %u\n", MyModule_X);
 
@@ -57,37 +61,36 @@ int n, i;
 }
 
 static ssize_t MyModule_read(struct file *filp, char *buff , size_t len, loff_t *off) {
+  
   // send from kernelSpace to userSpace
-
-  // todo check if the user can read
   uint8_t BufR[8];
   int i;
+  struct pData *pdata_p = filp->private_data;
 
   printk(KERN_WARNING"MyMod: READ\n");
 
   // retire content of ring buffer to local buffer une a la fois
   for(i = 0; i < len; i++){
-    cb_pop(filp->private_data->buf_rdwr, &BufR[i]);
+    cb_pop(&pdata_p->buf_rdwr, &BufR[i]);
   }
 
   printk(KERN_WARNING"MyMod: copied from ring buffer to local buffer \n");
 
-  copy_to_user(buff, &BufR, len);
-
-  printk(KERN_WARNING"MyMod: copied local buffer to user \n");
-
-
+  if(copy_to_user(buff, &BufR, len) == 0){
+    printk(KERN_WARNING "MyMod: All bytes have been sent to user");
+  } else {
+    printk(KERN_WARNING "MyMod: Some bytes have not been sent to user");
+    // TODO: implement the mechanism depending on blocking / non-blocking
+  }
   return 0;
- 
 }
 
 static ssize_t MyModule_write(struct file *filp, const char *buff , size_t len, loff_t *off) {
 
   // copy from userSpace to kernelSpace
-  // todo check if the user can write
-	//if(perso.file_read[perso.numPort]
   uint8_t BufW[8];
   int i;
+  struct pData *pdata_p = filp->private_data;
 
   printk(KERN_WARNING"MyMod: WRITE\n");
   copy_from_user(&BufW, buff, len);
@@ -98,54 +101,36 @@ static ssize_t MyModule_write(struct file *filp, const char *buff , size_t len, 
 
   // copy content of local buffer to ring buffer une a la fois
   for(i = 0; i < len; i++){
-    cb_push(filp->private_data->buf_rdwr, &BufW[i]);
+    cb_push(&pdata_p->buf_rdwr, &BufW[i]);
   }
-
-  
-
   return 0;
-
 }
 
 static int MyModule_open(struct inode *inode, struct file *filp) {
 
   uint32_t numPort;
+  struct pData *pdata_p;
+
+  printk(KERN_WARNING "MyMod: OPEN");
 
   // get the port the owner want to joint	
   numPort = MINOR(inode->i_rdev);
-
-  filp->private_data = &pData[numPort];
-
- 
-  // lock to protect critical code section
-  spin_lock(&lock_access);
-  // get the port the owner want to joint
-  portID = MINOR(inode->i_rdev);
-  printk(KERN_WARNING"MyMod: OPEN port: %u\n",portID);	
-  //perso[].owner = current_uid().val;
-  
+  pdata_p = &pdata[numPort];
+  filp->private_data = pdata_p;
 
   // TODO: Port Série doit être placé en mode Réception
 
-  //printk(KERN_WARNING "MyMod (end Open): file_read[%d]: %d file_write: %d owner: %d \n", numPort, perso.file_read[numPort], perso.file_write[numPort], perso.owner[numPort]);
-
   return 0;
-
 }
 
 static int MyModule_release(struct inode *inode, struct file *filp) {
-  uint32_t numPort;
+
 
   printk(KERN_WARNING"MyMod: RELEASE\n");
 
-  
-
   // TODO: Et si le mode d’ouverture était O_RDONL Y ou O_RDWR, la Réception du Port Série doit être interrompue afin d’arrêter de recevoir des données
 
-    //printk(KERN_WARNING "MyMod: (end Release) file_read[%d]: %d file_write: %d owner: %d \n", numPort, perso.file_read, perso.file_write, perso.owner);
-
-    return 0;
-
+  return 0;
 }
 
 
