c9b695148f4684b7bd7d9dda4f973640daceb3cf
Joschua Gosda <joschuagosda@Joschuas-MacBook-Air.local>
2022-10-28 18:33:03 -0400

    backup

--- Labo1/MyModule1/MyModule.c
+++ Labo1/MyModule1/MyModule.c
@@ -27,12 +27,23 @@ struct file_operations MyModule_fops = {
 };
 
 struct pData {
-  uint16_t owner; // takes the owners id
+  int8_t owner; // takes the owners id
   circular_buffer buf_rdwr;
   uint8_t numPort;
+  bool READ;
+  bool WRITE;
+  enum status
+  {
+    NONE = 0,
+    RD = O_RDONLY,
+    WR = O_WRONLY,
+    RDWR = O_RDWR
+  } mode;
 } pData;
 
 struct pData pdata[PORTNUMBER];
+
+bool portOpen[PORTNUMBER];
 
 struct cdev My_cdev;
 
@@ -53,6 +64,10 @@ static int __init mod_init (void) {
 
   for (n = 0; n < PORTNUMBER; n++){
     cb_init(&pdata[n].buf_rdwr, BUFFER_CAPACITY, BUFFER_ELEMENTSIZE);
+    pdata[n].mode = NONE;
+    pdata[n].READ = false;
+    pdata[n].WRITE = false;
+    pdata[n].owner = -1;
   }
 
   printk(KERN_WARNING"MyMod : Hello World ! MyModule_X = %u\n", MyModule_X);
@@ -67,7 +82,12 @@ static ssize_t MyModule_read(struct file *filp, char *buff , size_t len, loff_t 
   int i;
   struct pData *pdata_p = filp->private_data;
 
-  printk(KERN_WARNING"MyMod: READ\n");
+  // TODO: protection d'access
+  if (!((filp->f_flags & O_ACCMODE) == O_RDONLY || (filp->f_flags & O_ACCMODE) == O_RDWR)) {
+    return -EACCES;
+  }
+
+    printk(KERN_WARNING "MyMod: READ\n");
 
   // retire content of ring buffer to local buffer une a la fois
   for(i = 0; i < len; i++){
@@ -90,7 +110,16 @@ static ssize_t MyModule_write(struct file *filp, const char *buff , size_t len, 
   // copy from userSpace to kernelSpace
   uint8_t BufW[8];
   int i;
-  struct pData *pdata_p = filp->private_data;
+
+
+
+  // TODO: protection d'access
+  if (!((filp->f_flags & O_ACCMODE) == O_WRONLY || (filp->f_flags & O_ACCMODE) == O_RDWR))
+  {
+    return -EACCES;
+  }
+
+  struct pData *pdata_p= filp->private_data;
 
   printk(KERN_WARNING"MyMod: WRITE\n");
   copy_from_user(&BufW, buff, len);
@@ -116,10 +145,42 @@ static int MyModule_open(struct inode *inode, struct file *filp) {
   // get the port the owner want to joint	
   numPort = MINOR(inode->i_rdev);
   pdata_p = &pdata[numPort];
+
+  //printk(KERN_WARNING "MyMod: pdata_p->owner %d", pdata_p->owner);
+  //printk(KERN_WARNING "MyMod: currentuid  %u", current_uid().val);
+
+  if (pdata_p->owner < 0) {
+    pdata_p->owner = current_uid().val;
+    printk(KERN_WARNING "MyMod: user value changed to %u", pdata_p->owner);
+  }
+  else if (pdata_p->owner != current_uid().val)
+  {
+    printk(KERN_WARNING "MyMod: users are not the same");
+    return -EACCES;
+  }
+
+  // avoid open file 2 time in the same mode
+  if ((pdata_p->mode == (filp->f_flags & O_ACCMODE)) || pdata_p->mode == O_RDWR)
+  {
+    printk(KERN_WARNING "MyMod: E MODE");
+    return -EACCES;
+  }
+  // already a reader or a writer, so you become readwriter
+  else if (!(pdata_p->mode == NONE))
+  {
+    pdata_p->mode = RDWR;
+  }
+  // file was not opened before
+  else if ((filp->f_flags & O_ACCMODE) == O_RDONLY) {
+    pdata_p->mode = RD;
+  } else {
+    pdata_p->mode = WR;
+  }
+  
+
   filp->private_data = pdata_p;
-
   // TODO: Port Série doit être placé en mode Réception
-
+  printk(KERN_WARNING "MyMod: OPEN end\n");
   return 0;
 }
 
